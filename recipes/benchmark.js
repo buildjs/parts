99fc6b81c21d353b6a4c2580dac938f747b5ad10
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-21 09:39:06 +1000

    Added benchmark.js recipe

---
+++ recipes/benchmark.js
@@ -0,0 +1,8 @@
+# dsc: A benchmarking library that works on nearly all JavaScript platforms, supports high-resolution timers, and returns statistically significant results. As used on jsPerf.com.
+# author: bestiejs
+# url: http://benchmarkjs.com/
+# src: https://github.com/bestiejs/benchmark.js
+# bug: https://github.com/bestiejs/benchmark.js/issues
+
+[core]
+js <= github://bestiejs/benchmark.js/benchmark.js
