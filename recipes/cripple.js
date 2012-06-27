00b5f5a23ac3e596f47bea19e044ba6aa0283546
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-27 16:52:17 +1000

    Added cripple.js recipe

---
+++ recipes/cripple.js
@@ -0,0 +1,8 @@
+# dsc: Turn your Javascript Engine into a Blubbering Pile of IE8
+# author: DamonOehlman
+# url: null
+# src: https://github.com/DamonOehlman/cripple.js
+# bug: https://github.com/DamonOehlman/cripple.js/issues
+
+[core]
+js <= github://DamonOehlman/cripple.js/cripple.js
